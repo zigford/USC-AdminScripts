3f9edc4a4c33372da7cdefb958c9be54e8f51916
Jesse Harris <jpharris@usc.edu.au>
2018-04-19 11:10:39 +1000

    Fixed a mistake in comment help

--- ADScripts/Get-PwdLastSet.ps1
+++ ADScripts/Get-PwdLastSet.ps1
@@ -1,3 +1,46 @@
+<#
+    .SYNOPSIS
+    Get the date an account's password was last updated
+
+    .DESCRIPTION
+    Query AD for the account's PwdLastSet Attribute and convert it to a powershell date type
+
+    .PARAMETER Identity
+    The SamAccountName or Username of the account to query
+
+    .EXAMPLE
+    PS> .\Get-PwdLastSet.ps1 -Id jpharris
+
+    SamAccoutName  LastSet
+    -------------  -------
+    jpharris       9/01/2018 10:08:26 AM
+
+    .EXAMPLE
+    PS> Get-Content C:\UserList.txt | .\Get-PwdLastSet.ps1
+
+    SamAccountName  LastSet
+    --------------  -------
+    jpharris        9/01/2018 10:08:26 AM 
+    jphelan         20/12/2010 4:00:24 PM 
+    jphilli1        8/05/2017 1:12:01 PM  
+    jphillip        24/04/2016 8:56:31 AM 
+
+    .EXAMPLE
+    PS> .\Get-PwdLastSet.ps1 -Filter {samaccountname -like 'jph*'}
+
+    SamAccountName LastSet               
+    -------------- -------               
+    jph001         25/07/2013 1:21:14 PM 
+    jph002         24/04/2016 9:51:57 AM 
+    jph003         6/02/2011 10:19:54 AM 
+    jph009         6/02/2011 10:49:44 AM 
+    jph010         17/03/2014 10:16:48 PM
+
+    .NOTES
+    Author: Jesse Harris
+    Date: 19/04/2018
+#>
+
 [CmdLetBinding()]
 Param(
     [Parameter(
@@ -6,29 +49,6 @@ Param(
     [Parameter(
         ParameterSetName = "Filter")]$Filter
 )
-
-<#
-.SYNOPSIS
-Get the date an account's password was last updated
-
-.DESCRIPTION
-Query AD for the account's PwdLastSet Attribute and convert it to a powershell date type
-
-.PARAMETERS Identity
-The SamAccountName or Username of the account to query
-
-.EXAMPLE
-PS> Get-PwdLastSet -Id jpharris
-
-SamAccoutName  LastSet
--------------  -------
-jpharris       Tuesday Jan 4 2018
-
-.NOTES
-Author: Jesse Harris
-Date: 19/04/2018
-
-#>
 
 Begin {
     If (Get-Module -ListAvailable ActiveDirectory -EA SilentlyContinue) {
