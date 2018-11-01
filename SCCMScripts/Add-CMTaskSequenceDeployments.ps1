7d29cf9b6ab1e9b0205499074322b400c25bcb2a
Jesse Harris <jpharris@usc.edu.au>
2018-11-01 16:55:51 +1000

    Added some interactivity and more production like

--- SCCMScripts/Add-CMTaskSequenceDeployments.ps1
+++ SCCMScripts/Add-CMTaskSequenceDeployments.ps1
@@ -1,86 +1,106 @@
 ﻿[CmdLetBinding()]
-Param([Parameter(Mandatory=$True)]$TaskSequenceName,[Switch]$Whatif)
-#
-# Press 'F5' to run this script. Running this script will load the ConfigurationManager
-# module for Windows PowerShell and will connect to the site.
-#
-# This script was auto-generated at '20/12/2017 2:20:08 PM'.
-
-# Uncomment the line below if running in an environment where script signing is 
-# required.
-#Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+Param(
+    $TaskSequenceName,
+    [Parameter(Mandatory=$True)]
+    [ValidateScript(
+    {
+        Test-Path -Path $_
+    }
+    )][string]$DeploymentsCSV,
+    [Switch]$Whatif
+)
 
 # Site configuration
 $SiteCode = "SC1" # Site code 
-$ProviderMachineName = "wsp-configmgr01.usc.internal" # SMS Provider machine name
+$ProviderMachineName = "wsp-configmgr01.usc.internal" 
 
-# Customizations
-$initParams = @{}
-#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
-#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
-
-# Do not change anything below this line
-
-# Import the ConfigurationManager.psd1 module 
-if((Get-Module ConfigurationManager) -eq $null) {
-    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
+Function InsModule {
+    Param($ModuleName)
+    Switch ($ModuleName) {
+        'ConfigurationManager' {
+            if((Get-Module ConfigurationManager) -eq $null) {
+                Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
+            }
+        }
+        Default {
+            if((Get-Module $ModuleName) -eq $null) {
+                If (-Not (Get-Module -ListAvailable $ModuleName -ErrorAction Continue)) {
+                    Install-Module $ModuleName -Scope CurrentUser -Force
+                    Import-MOdule $ModuleName
+                }
+            
+            }
+        }
+    }
 }
+
+$RequiredModules = 'ConfigurationManager',
+    'USC-SCCM'
+
+$RequiredModules | ForEach-Object {
+    InsModule $PSItem
+}
+
+Connect-CfgSiteServer
 
 # Connect to the site's drive if it is not already present
 if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
     New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
 }
 Push-Location
-# Set the current location to be the site code.
-Set-Location "$($SiteCode):\" @initParams
 
-$TaskSequence = Get-CMTaskSequence -Name $TaskSequenceName
-$Deployments = @{
-    CollectionName = 'Mandatory Shared'
-    TaskSequencePackageId = $TaskSequence.PackageID
-    DeployPurpose = 'Required'
-    Availability = 'MediaAndPxe'
-    Schedule = (New-CMSchedule -Nonrecurring -Start (Get-Date))
-    ReRunBehavior = 'RerunIfFailedPreviousAttempt'
-    DeploymentOption = 'RunFromDistributionPoint'
-},@{
-    CollectionName = 'Mandatory NonShared'
-    TaskSequencePackageId = $TaskSequence.PackageID
-    DeployPurpose = 'Required'
-    Availability = 'MediaAndPxe'
-    Schedule = (New-CMSchedule -Nonrecurring -Start (Get-Date))
-    ReRunBehavior = 'RerunIfFailedPreviousAttempt'
-    DeploymentOption = 'RunFromDistributionPoint'
-},@{
-    CollectionName = 'Mandatory NonShared (No365Lic)'
-    TaskSequencePackageId = $TaskSequence.PackageID
-    DeployPurpose = 'Required'
-    Availability = 'MediaAndPxe'
-    Schedule = (New-CMSchedule -Nonrecurring -Start (Get-Date))
-    ReRunBehavior = 'RerunIfFailedPreviousAttempt'
-    DeploymentOption = 'RunFromDistributionPoint'
-},@{
-    CollectionName = 'Optional Shared'
-    TaskSequencePackageId = $TaskSequence.PackageID
-    DeployPurpose = 'Available'
-    Availability = 'ClientsMediaAndPxe'
-    DeploymentOption = 'RunFromDistributionPoint'
-},@{
-    CollectionName = 'All Unknown Computers'
-    TaskSequencePackageId = $TaskSequence.PackageID
-    DeployPurpose = 'Available'
-    Availability = 'ClientsMediaAndPxe'
-    DeploymentOption = 'RunFromDistributionPoint'
-},@{
-    CollectionName = 'Optional Image (Win10)'
-    TaskSequencePackageId = $TaskSequence.PackageID
-    DeployPurpose = 'Available'
-    Availability = 'ClientsMediaAndPxe'
-    DeploymentOption = 'RunFromDistributionPoint'
+Set-Location "$($SiteCode):\"
+if (-Not $TaskSequenceName) {
+    $TaskSequencesInDev = Get-CfgItemsByFolder -ItemType TaskSequence -FolderName Development 
+    $TSs = $TaskSequencesInDev | ForEach-Object { 
+        Get-CMTaskSequence -TaskSequencePackageId $PSItem.ObjectID
+    }
+    $i = 0
+    $pos = $TSs | ForEach-Object {
+        [PSCustomObject]@{
+            'Index' = $i
+            'TaskSequence' = $PSItem.Name
+        }
+        $i++
+    }
+    $quit = [PSCustomObject]@{'Index' = $i; 'TaskSequence' = 'QUIT '}
+    If ($pos -is [System.Array]) { $pos += $quit } else { $pos = @($pos, $quit) }
+    $TSSel = -1
+    While ($TSSel -lt 0 -or $TSSel -gt $i) {
+        $pos | Format-Table -AutoSize
+        $TSSel = Read-Host -Prompt "Please chose a task sequence or $i to quit)"
+        If ($TSSel -eq $i) { return }
+    }
+    $TaskSequence = $TSs[$TSSel]
+    $TaskSequenceName = $TaskSequence.Name
+} else {
+    $TaskSequence = Get-CMTaskSequence -Name $TaskSequenceName
 }
+
+$Now = New-CMSchedule -Nonrecurring -Start (Get-Date)
+
+Pop-Location
+
+$Deployments = Import-CSV -Path $DeploymentsCSV | ForEach-Object {
+    $HT = @{}
+    $PSItem.PSObject.Properties | ForEach-Object {
+        If ($PSItem.Value) {
+            $HT[$PSItem.Name] = $PSItem.Value
+        }
+    }
+    If ($HT['Schedule'] -eq 'Now') {
+        $HT['Schedule'] = $Now
+    }
+    $HT['TaskSequencePackageId'] = $TaskSequence.PackageID 
+    $HT
+}
+
+Push-Location
+Set-Location "$($SiteCode):\"
 
 $Deployments | ForEach-Object {
     Write-Verbose ("Creating deployment of {0} to {1} as {2}" -f $TaskSequenceName,$_.CollectionName,$_.DeployPurpose)
+    $_.ReRunBehavior
     $Object = [PSCustomObject]@{
         'TaskSequence' = $TaskSequenceName
         'CollectionName' = $_.CollectionName
