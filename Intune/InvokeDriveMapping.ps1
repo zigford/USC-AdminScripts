55be4d889f78b913e2b98b627fc9024fa53d4731
Jesse Harris <zigford@gmail.com>
2019-03-22 11:25:11 +1000

    Fixed issue with variables in string

--- Intune/InvokeDriveMapping.ps1
+++ Intune/InvokeDriveMapping.ps1
@@ -2,12 +2,10 @@ $repo = "https://raw.githubusercontent.com/zigford/USC-AdminScripts"
 $MappingScriptUrl = "${repo}/master/Intune/DriveMappingScript.ps1"
 $regKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 
-$Pwsh = 'PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -command '
-$Pwsh += "$([char]34)& {
-    `$Script = Invoke-RestMethod '$MappingScriptUrl'
-    `$Script = `$Script.Replace('ï','').Replace('»','').Replace('¿','')
-    `$Script | Invoke-Expression
-}$([char]34)"
+$Pwsh = 'PowerShell.exe -ExecutionPolicy Bypass -command '
+$Pwsh += "$([char]34)& {(Invoke-RestMethod '$MappingScriptUrl')"
+$Pwsh += ".Replace('ï','').Replace('»','').Replace('¿','')" 
+$Pwsh += "|Invoke-Expression}$([char]34)"
 
 if (-Not (Test-Path -Path $regKey)){
     New-ItemProperty -Path $regKey -Force
